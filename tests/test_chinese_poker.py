c3bd4831164ec280e8b81ade9b75ddacbfab080e
Matheus Couto <matheusccouto@gmail.com>
2020-06-13 13:11:35 -0300

    Change tests framework to pytest

--- tests/test_chinese_poker.py
+++ tests/test_chinese_poker.py
@@ -1,22 +1,23 @@
 """ Tests for the module chinese_poker. """
-import unittest
 
 import pandas as pd
+import pytest
 
 import poker
 from poker import chinese_poker
 
 
-class TestHand(unittest.TestCase):
+class TestHand:
     """ Test class Hand. """
 
-    def _test_royalties(self, hand_class, test_csv_file):
+    @staticmethod
+    def _test_royalties(hand_class, test_csv_file):
         data = pd.read_csv(test_csv_file, index_col=None)
         for row in data.itertuples():
             hand = hand_class(row.hand)
             royalties = hand.royalties()
             test_points = row.points
-            self.assertEqual(royalties, test_points)
+            assert royalties == test_points
 
     def test_top_royalties(self):
         """ Test top hand royalties. """
@@ -31,30 +32,31 @@ class TestHand(unittest.TestCase):
         self._test_royalties(chinese_poker.BottomHand, "test_bottom_royalties.csv")
 
 
-class TestPlayer(unittest.TestCase):
+class TestPlayer:
     """ Test class player. """
 
     def test_place_card_in_top_hand(self):
         player = chinese_poker.Player(name="Chris Moneymaker", points=2344)
         player.place_card(card=poker.Card('As'), hand='top')
-        self.assertEqual(len(player.top_hand), 1)
+        assert len(player.top_hand) == 1
 
     def test_place_card_in_mid_hand(self):
         player = chinese_poker.Player(name="Sam Farha", points=999)
         player.place_card(card=poker.Card('As'), hand='middle')
-        self.assertEqual(len(player.middle_hand), 1)
+        assert len(player.middle_hand) == 1
 
     def test_place_card_in_btm_hand(self):
         player = chinese_poker.Player(name="Dan Harrington", points=574)
         player.place_card(card=poker.Card('As'), hand='bottom')
-        self.assertEqual(len(player.bottom_hand), 1)
+        assert len(player.bottom_hand) == 1
 
 
-class TestGame(unittest.TestCase):
+class TestGame:
     """ Test game dynamics."""
 
     @staticmethod
     def _new_round():
+        """ Start a new round. """
         pkr = chinese_poker.Poker(n_seats=2)
         chris = chinese_poker.Player(name="Chris Moneymaker", points=0)
         sam = chinese_poker.Player(name="Sam Farha", points=0)
@@ -62,10 +64,10 @@ class TestGame(unittest.TestCase):
         return pkr.new_round()
 
     def test_round(self):
+        """
+        Test initializing a new round. Check if number of cards is
+        correct.
+        """
         rnd = self._new_round()
         p1 = rnd.players[0]
-        self.assertEqual(len(p1.hand), 13)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        assert len(p1.hand) == 13
--- tests/test_poker.py
+++ tests/test_poker.py
@@ -1,13 +1,13 @@
 """ Tests for poker module. """
 
 import itertools
-import unittest
 
 import pandas as pd
+import pytest
 
 import poker
 
-TEST_HANDS = pd.read_csv("tests\\test_hands.csv", index_col=None)
+TEST_HANDS = pd.read_csv("test_hands.csv", index_col=None)
 
 # Here we shall honor the 2003 WSOP finalists.
 p1 = poker.Player(name="Chris Moneymaker", chips=2344)
@@ -16,7 +16,7 @@ p3 = poker.Player(name="Dan Harrington", chips=574)
 players = (p1, p2, p3)
 
 
-class TestCard(unittest.TestCase):
+class TestCard:
     """ Test the class card. """
 
     RANKS = ("2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A")
@@ -26,71 +26,75 @@ class TestCard(unittest.TestCase):
         """ Test lower and upper case suits. """
         lowercase_card = poker.Card("As")
         uppercase_card = poker.Card("AS")
-        self.assertEqual(lowercase_card.suit, uppercase_card.suit)
+        assert lowercase_card.suit == uppercase_card.suit
 
     def test_case_rank(self):
         """ Test lower and upper case ranks. """
         lowercase_card = poker.Card("As")
         uppercase_card = poker.Card("as")
-        self.assertEqual(lowercase_card.rank, uppercase_card.rank)
+        assert lowercase_card.rank == uppercase_card.rank
 
     def test_all_ranks(self):
         """ Test if all ranks are recognized. """
         for rank in self.RANKS:
             card = poker.Card(f"{rank}s")
-            self.assertEqual(card.rank, rank)
+            assert card.rank == rank
 
     def test_numerical_rank(self):
         """ Test if ranks are converted to numerical ranks correctly. """
         numerical_ranks = range(2, 15)
         for rank, numerical_rank in zip(self.RANKS, numerical_ranks):
             card = poker.Card(f"{rank}s")
-            self.assertEqual(card.numerical_rank, numerical_rank)
+            assert card.numerical_rank == numerical_rank
 
     def test_all_suits(self):
         """ test if all suits are recognized."""
         for suit in self.SUITS:
             card = poker.Card(f"A{suit}")
-            self.assertEqual(card.suit, suit)
+            assert card.suit == suit
 
     def test_rank_value_error(self):
         """ Test if invalid ranks raise exceptions. """
         invalid_ranks = ("0", "1", "10")
         for rank in invalid_ranks:
-            self.assertRaises(ValueError, poker.Card, f"{rank}s")
+            with pytest.raises(ValueError):
+                poker.Card(f"{rank}s")
 
     def test_suit_value_error(self):
         """ "Test if invalid suits raise exceptions. """
         invalid_suits = ("spades", "hearts", "clubs", "diamonds", "x", "y", "z")
         for suit in invalid_suits:
-            self.assertRaises(ValueError, poker.Card, f"A{suit}")
+            with pytest.raises(ValueError):
+                poker.Card(f"A{suit}")
 
     def test_repr(self):
         """ Test class' __repr__. """
         for rank, suit in itertools.product(self.RANKS, self.SUITS):
-            self.assertEqual(rank + suit, repr(poker.Card(rank + suit)))
+            assert rank + suit == repr(poker.Card(rank + suit))
 
 
-class TestHand(unittest.TestCase):
+class TestHand:
     """ Test class Hand. """
 
     def test_create_empty_hand(self):
         """ Test the creation of an empty hand. """
         hand = poker.Hand()
-        self.assertEqual(hand.ranks, [])
-        self.assertEqual(hand.suits, [])
-        self.assertEqual(hand.numerical_ranks, [])
-        self.assertEqual(hand.value, 0)
+        assert hand.ranks == []
+        assert hand.suits == []
+        assert hand.numerical_ranks == []
+        assert hand.value == 0
 
     def check_hand_ranking_method(self, method: str):
         """ Helper function to check the is_{ranking} methods. """
         for row in TEST_HANDS.itertuples():
-            # Remove the "is_" from the beginning of the method name to compare with the ranking from the file.
-            # If is the same ranking that the method is testing, the result must be true, otherwise must be false.
+            # Remove the "is_" from the beginning of the method name to
+            # compare with the ranking from the file. If is the same
+            # ranking that the method is testing, the result must be
+            # true, otherwise must be false.
             if row.ranking == method[3:]:
-                self.assertTrue(getattr(poker.Hand(row.hand), method)())
+                assert getattr(poker.Hand(row.hand), method)()
             else:
-                self.assertFalse(getattr(poker.Hand(row.hand), method)())
+                assert not getattr(poker.Hand(row.hand), method)()
 
     def test_is_high_card(self):
         """ Test is_high_card method. """
@@ -135,11 +139,17 @@ class TestHand(unittest.TestCase):
     def test_value(self):
         """ Test the hand value. """
         for row in TEST_HANDS.itertuples():
-            self.assertEqual(poker.Hand(row.hand).value, int(row.value))
+            assert poker.Hand(row.hand).value == int(row.value)
 
     def test_string_arguments(self):
         """" Test creation of a hand by string arguments. """
-        reference = poker.Hand(poker.Card("Ad"), poker.Card("Ks"), poker.Card("Tc"), poker.Card("6c"), poker.Card("2h"))
+        reference = poker.Hand(
+            poker.Card("Ad"),
+            poker.Card("Ks"),
+            poker.Card("Tc"),
+            poker.Card("6c"),
+            poker.Card("2h")
+        )
         alternatives = [
             poker.Hand("Ad", "Ks", "Tc", "6c", "2h"),
             poker.Hand("AdKsTc6c2h"),
@@ -148,19 +158,23 @@ class TestHand(unittest.TestCase):
             poker.Hand("Ad,Ks,Tc,6c,2h"),
         ]
         for alt in alternatives:
-            self.assertEqual(reference.value, alt.value)
+            assert reference.value == alt.value
 
     def test_repr(self):
         """ Test class' __repr__. """
         for row in TEST_HANDS.itertuples():
-            # Since the repr from Hand sorts the hand. Comparing the repr to the hand string in the test_hands.csv
-            # would fail. The way I decided to check if it was working was to construct another Hand from the repr.
-            # If the repr is working correctly, it must construct a Hand instance with the same repr as the hand
-            # instance from the hand created by with the test_hand.csv.
-            self.assertEqual(repr(poker.Hand(repr(poker.Hand(row.hand)))), repr(poker.Hand(row.hand)))
+            # Since the repr from Hand sorts the hand. Comparing the
+            # repr to the hand string in the test_hands.csv would fail.
+            # The way I decided to check if it was working was to
+            # construct another Hand from the repr. If the repr is
+            # working correctly, it must construct a Hand instance with
+            # the same repr as the hand instance from the hand created
+            # by with the test_hand.csv.
+            assert (repr(poker.Hand(repr(poker.Hand(row.hand))))
+                    == repr(poker.Hand(row.hand)))
 
 
-class TestPoker(unittest.TestCase):
+class TestPoker:
     """  Test the class Poker. """
 
     def test_add_player(self):
@@ -170,72 +184,77 @@ class TestPoker(unittest.TestCase):
         player = players[0]
         pkr.add_player(player, seat=1)
         n_players_after = len([seat for seat in pkr.seats if seat is not None])
-        self.assertEqual(n_players_before + 1, n_players_after)
+        assert n_players_before + 1 == n_players_after
 
     def test_remove_player(self):
         """ Test removing a player. """
         pkr = poker.Poker(n_seats=len(players))
         pkr.add_players(players)
         pkr.remove_player(0)
-        self.assertEqual(pkr.seats[0], None)
+        assert pkr.seats[0] == None
 
     def test_set_dealer(self):
-        """" Test that a new dealer is set every time a new game is started. """
-        # Dealer is set randomly, so there is no safe way of checking it. To overcome this I generate several Poker
-        # instances and check if at least in one of them the dealer is different. It is not proof-leak, one may be so
-        # unlucky that all values are randomly the same. However, the chances are very low, and whenever this happens,
-        # when testing again it should work.
+        """
+        Test that a new dealer is set every time a new game is started.
+        """
+        # Dealer is set randomly, so there is no safe way of checking
+        # it. To overcome this I generate several Poker instances and
+        # check if at least in one of them the dealer is different. It
+        # is not proof-leak, one may be so unlucky that all values are
+        # randomly the same. However, the chances are very low, and
+        # whenever this happens, when testing again it should work.
         pokers = [poker.Poker().dealer for _ in range(10)]
-        self.assertGreater(len(set(pokers)), 1)
+        assert len(set(pokers)) > 1
 
     def test_raise_seat_occupied(self):
-        """ Test if SeatOccupiedError is trown when necessary. """
+        """ Test if SeatOccupiedError is thrown when necessary. """
         pkr = poker.Poker()
         pkr.add_player(players[0], seat=0)
-        self.assertRaises(poker.SeatOccupiedError, pkr.add_player, player=players[1], seat=0)
+        with pytest.raises(poker.SeatOccupiedError):
+            pkr.add_player(player=players[1], seat=0)
 
 
-class TestRound(unittest.TestCase):
+class TestRound:
 
     def test_deal_cards_to_all_players(self):
         """ Test dealing cards to all players. """
         n_starting_cards = 5
-        round = poker.Round(players=players, n_starting_cards=n_starting_cards)
+        rnd = poker.Round(players=players, n_starting_cards=n_starting_cards)
 
-        for player in round.players:
-            self.assertEqual(player.hand.n_cards, n_starting_cards)
+        for player in rnd.players:
+            assert player.hand.n_cards == n_starting_cards
 
     def test_deal_cards_to_one_player(self):
         """ Test dealing cards to a single player only. """
         n_starting_cards = 5
         n_cards_to_deal = 2
-        round = poker.Round(players=players, n_starting_cards=n_starting_cards)
+        rnd = poker.Round(players=players, n_starting_cards=n_starting_cards)
 
         # Deal cards to a single player
-        player_to_receive = round.players[0]
-        round.deal_cards(player=player_to_receive, n_cards=n_cards_to_deal)
+        player_to_receive = rnd.players[0]
+        rnd.deal_cards(player=player_to_receive, n_cards=n_cards_to_deal)
 
-        for player in round.players:
+        for player in rnd.players:
             if player is player_to_receive:
-                self.assertEqual(player.hand.n_cards, n_starting_cards + n_cards_to_deal)
+                assert player.hand.n_cards == n_starting_cards + n_cards_to_deal
             else:
-                self.assertEqual(player.hand.n_cards, n_starting_cards)
+                assert player.hand.n_cards == n_starting_cards
 
     def test_winner(self):
         """ Test the method winner. """
         test_hands = (poker.Hand('As Ah 4d Tc Js'), poker.Hand('3s 4h 5d 6c 7s'), poker.Hand('Qs Ah 4d Tc Js'))
         winner = 1
         n_starting_cards = 5
-        round = poker.Round(players=players, n_starting_cards=n_starting_cards)
+        rnd = poker.Round(players=players, n_starting_cards=n_starting_cards)
 
         # Access the players hand to force they have the test hands.
-        for player, test_hand in zip(round.players, test_hands):
+        for player, test_hand in zip(rnd.players, test_hands):
             player.hand = test_hand
 
-        self.assertEqual(round.winner(), winner)
+        assert rnd.winner() == winner
 
 
-class TestDeck(unittest.TestCase):
+class TestDeck:
     """ Test the Deck class"""
 
     @staticmethod
@@ -247,20 +266,17 @@ class TestDeck(unittest.TestCase):
     def test_raise_not_enough_cards(self):
         """ Test that NotEnoughCards is being raised when necessary. """
         deck = poker.Deck()
-        self.assertRaises(poker.NotEnoughCardsError, self.draw_many_cards, deck)
+        with pytest.raises(poker.NotEnoughCardsError):
+            self.draw_many_cards(deck)
 
     def test_set(self):
         """ Test if deck is being set correctly. """
         deck = poker.Deck()
-        self.assertEqual(len(deck.cards), 52)
+        assert len(deck.cards) == 52
 
     def test_shuffle(self):
         """ Test if method set and shuffle"""
         deck = poker.Deck()
         cards = deck.cards.copy()
         deck.set_and_shuffle()
-        self.assertNotEqual(cards, deck.cards)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        assert cards != deck.cards
